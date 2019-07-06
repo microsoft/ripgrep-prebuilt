1461453b78b5999e6d5243d9d30c659bab116355
Rob Lourens <roblourens@gmail.com>
2019-07-05 18:19:56 -0700

    Fix upload

--- build/build_ppc.sh
+++ build/build_ppc.sh
@@ -4,13 +4,15 @@ set -ex
 
 REPO=$(node -p "require('./config.json').ripgrepRepo")
 TREEISH=$(node -p "require('./config.json').ripgrepTag")
+THIS_TAG=`git tag -l --contains HEAD`
 
 cd ~
 git clone https://github.com/${REPO}.git
 cd ripgrep
 git checkout $TREEISH
-cargo build --release --target=powerpc64le-unknown-linux-gnu --features 'pcre2'
-strip ./target/powerpc64le-unknown-linux-gnu/release/rg
-zip -j "ripgrep-linux-ppc64le.zip" ./target/powerpc64le-unknown-linux-gnu/release/rg
-target/powerpc64le-unknown-linux-gnu/release/rg --version
 
+TARGET="powerpc64le-unknown-linux-gnu"
+cargo build --release --target=$TARGET --features 'pcre2'
+strip ./target/${TARGET}/release/rg
+zip -j "ripgrep-${THIS_TAG}-${TARGET}.zip" ./target/${TARGET}/release/rg
+target/${TARGET}/release/rg --version
