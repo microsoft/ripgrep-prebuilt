06dca9f62f21bfb9ff0cbd3ede815042cb525bba
Rob Lourens <roblourens@gmail.com>
2019-07-19 09:51:21 -0700

    Flatten path in ppc tar as well

--- build/build_ppc.sh
+++ build/build_ppc.sh
@@ -14,5 +14,5 @@ git checkout $TREEISH
 TARGET="powerpc64le-unknown-linux-gnu"
 cargo build --release --target=$TARGET --features 'pcre2'
 strip ./target/${TARGET}/release/rg
-tar czvf "ripgrep-${THIS_TAG}-${TARGET}.tar.gz" ./target/${TARGET}/release/rg
+tar czvf "ripgrep-${THIS_TAG}-${TARGET}.tar.gz" -C ./target/${TARGET}/release/ rg
 target/${TARGET}/release/rg --version
