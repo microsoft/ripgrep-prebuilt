1fc11eb4cee5b347e0970faad08f28508d4cc59b
Rob Lourens <roblourens@gmail.com>
2021-12-28 10:29:35 -0800

    Fix #15, always run strip

--- build/package.sh
+++ build/package.sh
@@ -14,10 +14,9 @@ mk_tarball() {
     local name="ripgrep-${this_tag}-${TARGET}.tar.gz"
     # When cross-compiling, use the right `strip` tool on the binary.
     local gcc_prefix="$(gcc_prefix)"
-    if [ -n "${gcc_prefix}" ]; then
-        # Copy the ripgrep binary and strip it.
-        "${gcc_prefix}strip" "target/$TARGET/release/rg"
-    fi
+    
+    # Copy the ripgrep binary and strip it.
+    "${gcc_prefix}strip" "target/$TARGET/release/rg"
 
     tar czvf "$OUT_DIR/$name" -C ./target/$TARGET/release rg
     echo "##vso[task.setvariable variable=Name]$name"
