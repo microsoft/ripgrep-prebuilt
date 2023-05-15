4115e8959b6dddb43b31aac130fbeb8d37d8815a
andreamah <andrea_mah22@hotmail.com>
2023-05-15 14:14:45 -0700

    change handling env vars

--- build/build.sh
+++ build/build.sh
@@ -10,25 +10,24 @@ set -ex
 main() {
     CARGO="$(builder)"
 
-    ENV_VARS=""
+    PAGESIZE_ARG_VAR=""
     if is_linux && is_musl; then
         # jemalloc doesn't allow 16K page sizes for linux musl 
         # https://github.com/microsoft/ripgrep-prebuilt/issues/26
-        ENV_VARS="JEMALLOC_SYS_WITH_LG_PAGE=14;"
+        PAGESIZE_ARG_VAR="JEMALLOC_SYS_WITH_LG_PAGE=14"
     fi
 
     # Test a normal debug build.
     if is_arm || is_aarch64 || is_ppc64le; then
-        $ENV_VARS "$CARGO" build --target "$TARGET" --release --features 'pcre2'
+        $PAGESIZE_ARG_VAR "$CARGO" build --target "$TARGET" --release --features 'pcre2'
     # pcre2 is not supported on s390x
     # https://github.com/zherczeg/sljit/issues/89
     elif is_s390x; then
-        $ENV_VARS "$CARGO" build --release --target=$TARGET
+        $PAGESIZE_ARG_VAR "$CARGO" build --release --target=$TARGET
     else
-        ENV_VARS="PCRE2_SYS_STATIC=1; $ENV_VARS";
         # Technically, MUSL builds will force PCRE2 to get statically compiled,
         # but we also want PCRE2 statically build for macOS binaries.
-        $ENV_VARS "$CARGO" build --target "$TARGET" --release --features 'pcre2'
+        PCRE2_SYS_STATIC=1 $PAGESIZE_ARG_VAR "$CARGO" build --target "$TARGET" --release --features 'pcre2'
     fi
 
     # Show the output of the most recent build.rs stderr.
